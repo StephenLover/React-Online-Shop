a054442448787c7b4a0e68ec0d1099dccc288a66
Stephen Wang <paradoxstephen@gmail.com>
2019-07-25 15:07:01 +1000

    react-router-dom added, basic router set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import './App.css';
 
+import { Link, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component'
+
+const HatsPage = props => (
+  <div>
+    <h1>
+      HATS PAGE
+    </h1>
+  </div>
+)
+
+const TopicsList = props => (
+  <div>
+    <h1>
+      HATS PAGE
+    </h1>
+  </div>
+)
+
+const TopicDetail = props => (
+  <div>
+    <h1>
+      HATS PAGE
+    </h1>
+  </div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Route exact={true} path='/' component={HomePage} />
+      <Route exact={true} path='/topics' component={TopicsList} />
+      <Route exact={true} path='/topics/:topicId' component={HatsPage} />
     </div>
   );
 }
