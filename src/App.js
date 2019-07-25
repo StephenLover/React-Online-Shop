1e1d1ded91becb1c475fb5683596fc18eff4b040
Stephen Wang <paradoxstephen@gmail.com>
2019-07-25 22:08:22 +1000

    updating the shop page and relative components

--- src/App.js
+++ src/App.js
@@ -4,37 +4,13 @@ import './App.css';
 import { Link, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component'
-
-const HatsPage = props => (
-  <div>
-    <h1>
-      HATS PAGE
-    </h1>
-  </div>
-)
-
-const TopicsList = props => (
-  <div>
-    <h1>
-      HATS PAGE
-    </h1>
-  </div>
-)
-
-const TopicDetail = props => (
-  <div>
-    <h1>
-      HATS PAGE
-    </h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div>
       <Route exact={true} path='/' component={HomePage} />
-      <Route exact={true} path='/topics' component={TopicsList} />
-      <Route exact={true} path='/topics/:topicId' component={HatsPage} />
+      <Route path='/shop' component={ShopPage} />
     </div>
   );
 }
