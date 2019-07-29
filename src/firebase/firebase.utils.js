a1ee6e44b2834b9788f60d05d85d5061223a84f8
Stephen Wang <paradoxstephen@gmail.com>
2019-07-29 11:45:50 +1000

    finishing sign in handleSubmit method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const config = {
   appId: "1:568550086020:web:94a2b1e51490788d"
 }
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef(userRef)
+
+  if (!snapShot.exists) {
+    const {
+      displayName,
+      email
+    } = userAuth;
+    const createAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+}
+
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
