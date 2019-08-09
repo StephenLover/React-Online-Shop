2a19531a60fc967a0fe637f75231b7cdd362346d
Stephen Wang <paradoxstephen@gmail.com>
2019-08-09 16:04:38 +1000

    added redux, and implemented userReducer and userAction to header and App

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,38 +1,33 @@
 import React from 'react';
 
-import './sign-in.styles.scss'
-
-import FormInput from '../form-input/form-input.component'
+import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
+import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
+
     this.state = {
       email: '',
       password: ''
     };
   }
 
-  handleSubmit = async event => {
+  handleSubmit = event => {
     event.preventDefault();
 
-    const { email, password } = this.state;
-
-    try {
-      await auth.signInWithEmailAndPassword(email, password);
-      this.setState({ email: '', password: '' });
-    } catch (error) {
-      console.log(error)
-    }
-  }
+    this.setState({ email: '', password: '' });
+  };
 
   handleChange = event => {
     const { value, name } = event.target;
-    this.setState({ [name] : value})
-  }
+
+    this.setState({ [name]: value });
+  };
 
   render() {
     return (
@@ -59,12 +54,14 @@ class SignIn extends React.Component {
           />
           <div className='buttons'>
             <CustomButton type='submit'> Sign in </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn> SignIn with Google </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
           </div>
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default SignIn
+export default SignIn;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -7,38 +7,40 @@ const config = {
   authDomain: "crwn-clothing-db-5e035.firebaseapp.com",
   databaseURL: "https://crwn-clothing-db-5e035.firebaseio.com",
   projectId: "crwn-clothing-db-5e035",
-  storageBucket: "",
+  storageBucket: "crwn-clothing-db-5e035.appspot.com",
   messagingSenderId: "568550086020",
   appId: "1:568550086020:web:94a2b1e51490788d"
-}
+};
+
+firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
-  const snapShot = await userRef(userRef)
+
+  const snapShot = await userRef.get();
 
   if (!snapShot.exists) {
     const {
       displayName,
       email
     } = userAuth;
-    const createAt = new Date();
+    const createdAt = new Date();
     try {
       await userRef.set({
         displayName,
         email,
-        createAt,
+        createdAt,
         ...additionalData
-      })
+      });
     } catch (error) {
       console.log('error creating user', error.message);
     }
   }
-}
 
-
-firebase.initializeApp(config);
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
